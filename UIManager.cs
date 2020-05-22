12d44095e7fd4633e3bb87c51fc94fc2e6478459
Mark-James McDougall <contact@markjames.dev>
2020-05-21 23:21:25 -0400

    Initial commit of UIManager class

---
+++ UIManager.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using SadConsole;
+namespace SadConsoleRLTutorial
+{
+    // Creates/Holds/Destroys all consoles used in the game
+    // and makes consoles easily addressable from a central place.
+    public class UIManager : ContainerConsole
+    {
+        public ScrollingConsole MapConsole;
+
+        public UIManager()
+        {
+            // must be set to true
+            // or will not call each child's Draw method
+            IsVisible = true;
+            IsFocused = true;
+
+            // The UIManager becomes the only
+            // screen that SadConsole processes
+            Parent = SadConsole.Global.CurrentScreen;
+        }
+    }
+}
